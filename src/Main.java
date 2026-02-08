a3c02ac75e76c5117a11915b908e3cb36d034eee
Sarsenov <zhanbek.sarsenov@gmail.com>
2026-02-09 02:07:26 +0500

    Реалізує завдання

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class Main {
+    private static Map<String, String> fillMap() {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("Washington", "Trump");
+        map.put("London",     "Holmes");
+        map.put("Kiev",       "Zelensky");
+        map.put("Kharkiv",    "Sapronov");
+        map.put("Odessa",     "Deliev");
+
+        return map;
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> map = fillMap();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        System.out.print("Введіть місто або натисніть клавішу \"S\", щоб завершити програму: ");
+        String city = scanner.next();
+
+        while (!city.equalsIgnoreCase("S")) {
+            String lastName = map.get(city);
+            System.out.println( (lastName != null) ? lastName : "В даному місті ніхто не знайдений. Будь ласка, повторіть спробу!" );
+
+            System.out.println();
+            System.out.print("Введіть місто або натисніть клавішу \"S\", щоб завершити програму: ");
+            city = scanner.next();
+
+        }
     }
 }
